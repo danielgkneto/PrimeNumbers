9fbc4d230172a88c335b10c7fdb3661cfe70f2e2
danielgkneto <GBTC440016UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-09-06 13:59:33 -0400

    Fix an error that makes number 4 showing as prime

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,37 @@
-//Write an application that will show the number and indicate whether or not it is prime. There are two ways that you can achieve this.
-
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
-        int number;
-        Random random = new Random();
 
-        number = random.nextInt(250);
+        Scanner kb = new Scanner(System.in);
+        int answer;
 
-        System.out.println(number + (isPrime(number) ? " is" : " is not") + " a prime number.");
+        System.out.println("\nPrime numbers\n\nChoose:\n\n1) Find out if a random number (0-250) is prime or not\n2) Find out all prime numbers up to the number of your choice\n");
+        answer = kb.nextInt();
+
+        if (answer == 1) {
+            int number;
+            Random random = new Random();
+
+            number = random.nextInt(250);
+
+            System.out.println(number + (isPrime(number) ? " is" : " is not") + " a prime number.");
+        }
+        else if (answer == 2) {
+            System.out.println("Enter a number: ");
+            int number = kb.nextInt();
+
+            for (int i = 0; i <= number; i++){
+                System.out.println(i + (isPrime(i) ? " is" : " is not") + " a prime number.");
+            }
+        }
     }
 
     public static boolean isPrime(int number) {
         if (number <= 1) return false;
-        for (int i = 2; i < Math.sqrt(number); i++) {
+        for (int i = 2; i <= Math.sqrt(number); i++) {
             if (number % i == 0) return false;
         }
 
